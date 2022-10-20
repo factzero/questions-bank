09e1fe2c2e84115354e4c2ee0b98b713a67e332c
factzero <azlinzhi@aliyun.com>
2022-10-20 22:38:36 +0800

    init

---
+++ web/src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// 引入svg注册脚本
+import 'virtual:svg-icons-register';
+
+// 自定义样式
+import '@/styles/index.scss';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
